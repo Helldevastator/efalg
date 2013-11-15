0f67e65508ea6f14ae594d0c024ad191dd824a0d
Jonas Schwammberger <hooovercat@gmail.com>
2013-11-15 11:08:10 +0100

    more stuff has happened

--- efalg_task5_2/src/WinterVertex.java
+++ efalg_task5_2/src/WinterVertex.java
@@ -1,5 +1,3 @@
-package efalg_task5_2;
-
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -9,20 +7,19 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class WinterVertex {
-	static ArrayList<ArrayList<Connection>> timeTable;
-	static ArrayList<Vertex> cities;
 	static HashMap<Long, Node> graph;
 	static int max = Integer.MAX_VALUE - 100000000;
+	static int startCity = 0;
+	static long startNode = 0;
 
 	public static void main(String[] args) throws Exception {
 		Scanner in = new Scanner(new File("winter.in"));
 
 		int cityCount = in.nextInt();
 		int connCount = in.nextInt();
-		long[] currentCities = new long[cityCount];
-		ArrayList<Long> allVertices = new ArrayList<>();
+		ArrayList<Long> allVertices = new ArrayList<>(cityCount * 25);
 
-		graph = new HashMap<>();
+		graph = new HashMap<>(cityCount * 25);
 
 		for (int i = 0; i < connCount; i++) {
 			long depart = in.nextLong();
@@ -30,49 +27,62 @@ public class WinterVertex {
 			long arrival = in.nextLong();
 			int dest = in.nextInt();
 
-			long start = encode(depart, city);
-			long end = encode(arrival, dest);
+			long start = encode(depart, city - 1);
+			long end = encode(arrival, dest - 1);
 			if (!graph.containsKey(start)) {
 				graph.put(start, new Node(start));
+			}
+
+			if (!graph.containsKey(end)) {
+				graph.put(end, new Node(end));
 			}
 			Node n = graph.get(start);
 
 			n.connections.add(end);
 			allVertices.add(start);
-
-			//Vertex v = cities.get(city - 1);
-
-			//v.connections.add(new Connection(depart, arrival, dest - 1));
+			allVertices.add(end);
 		}
 
 		Collections.sort(allVertices);
+		startNode = -1;
+		startCity = 0;
 
 		//add waiting times
 		long[] tmp = new long[cityCount];
+
+		for (int i = 0; i < cityCount; i++) {
+			tmp[i] = -1;
+		}
+
 		for (int i = 0; i < allVertices.size(); i++) {
-			int city = decodeCity(allVertices.get(i));
-			if (tmp[city - 1] != 0) {
-				Node n = graph.get(allVertices.get(i));
-				n.connections.add(tmp[city - 1]);
+			int index = decodeCity(allVertices.get(i));
+			if (index == 0 && startNode < 0) {
+				startNode = allVertices.get(i);
 			}
-			tmp[city - 1] = allVertices.get(i);
+
+			if (tmp[index] > -1) {
+				Node before = graph.get(tmp[index]);
+				before.connections.add(allVertices.get(i));
+			}
+			tmp[index] = allVertices.get(i);
 		}
 
 		//dijkstra
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		Node start = new Node(new Long(1));
+		PriorityQueue<Node> pq = new PriorityQueue<>(cityCount);
+		Node start = graph.get(startNode);
 		start.cost = 0;
 		pq.add(start);
 
-		int minTime = 0;
+		int minTime = -1;
 		while (!pq.isEmpty()) {
 			Node current = pq.poll();
 			current.visited = true;
+
 			int city = decodeCity(current.v);
 			int time = decodeTime(current.v);
 			ArrayList<Long> conn = current.connections;
 
-			if (city == cityCount) {
+			if (city == cityCount - 1) {
 				minTime = current.cost;
 				break;
 			}
@@ -81,37 +91,44 @@ public class WinterVertex {
 				Node destination = graph.get(conn.get(i));
 				int destTime = decodeTime(destination.v);
 				int destCity = decodeCity(destination.v);
+				if (destTime >= time) {
+					int waitTime = destCity == city ? destTime - time : 0;
+					if (city == startCity && destCity == startCity) {
+						waitTime = 0;
+					}
+					int accCost = current.cost + waitTime;
 
-				int waitTime = destCity == city ? destTime - time : 0;
-				int accCost = current.cost + waitTime;
-
-				if (accCost < destination.cost && !destination.visited) {
-					destination.cost = accCost;
-					pq.add(destination);
+					if (accCost < destination.cost && !destination.visited) {
+						destination.cost = accCost;
+						pq.add(destination);
+					}
 				}
 			}
 
 		}
 
-		System.out.println(minTime);
+		//System.out.println(minTime);
 		PrintWriter out = new PrintWriter("winter.out");
-		out.write(minTime);
+		out.print(minTime);
 		out.close();
 	}
 
 	public static long encode(long time, int city) {
 		long start = time;
 		start = start << 16;
+		//start = start * 10000;
 		start += city;
 		return start;
 	}
 
 	public static int decodeCity(long in) {
 		return (int) (in & 0xffff);
+		//return (int) (in % 10000);
 	}
 
 	public static int decodeTime(long in) {
 		return (int) (in >>> 16);
+		//return (int) (in / 10000);
 	}
 
 	public static class Node implements Comparable<Node> {
@@ -123,52 +140,13 @@ public class WinterVertex {
 		public Node(Long v) {
 			this.v = v;
 			cost = max;
-			connections = new ArrayList<>();
+			connections = new ArrayList<>(5);
 		}
 
 		@Override
 		public int compareTo(Node o) {
 			return Integer.compare(cost, o.cost);
 
-		}
-
-	}
-
-	public static class Vertex implements Comparable<Vertex> {
-		int city;
-		int waitingTime;
-		long currentTime;
-		ArrayList<Connection> connections;
-
-		public Vertex(int city) {
-			this.city = city;
-			currentTime = -1;
-			waitingTime = max;
-			connections = new ArrayList<>();
-		}
-
-		@Override
-		public int compareTo(Vertex o) {
-			int x = Integer.compare(waitingTime, o.waitingTime);
-			return x == 0 ? Integer.compare(o.city, city) : x;
-		}
-	}
-
-	public static class Connection implements Comparable<Connection> {
-		long departTime;
-		long arrivalTime;
-		int destCity;
-
-		public Connection(long depart, long arrival, int destCity) {
-			this.departTime = depart;
-			this.arrivalTime = arrival;
-			this.destCity = destCity;
-
-		}
-
-		@Override
-		public int compareTo(Connection o) {
-			return Long.compare(departTime, o.departTime);
 		}
 
 	}
