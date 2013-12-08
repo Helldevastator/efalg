a869a1a7ca4ca8bee1a359699ec1085d0d638322
Jonas Schwammberger <hooovercat@gmail.com>
2013-12-08 15:15:53 +0100

    making progress on reading files

--- efalg_uebung5/src/ch/fhnw/efalg/schwammberger/jonas/uebung5/Source.java
+++ efalg_uebung5/src/ch/fhnw/efalg/schwammberger/jonas/uebung5/Source.java
@@ -2,13 +2,15 @@ package ch.fhnw.efalg.schwammberger.jonas.uebung5;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Source {
 
 	public static void main(String[] args) {
 		if (args.length == 0) {
-			testTwoPhaseSimplex();
+			testSimpleSimplex();
+			//testTwoPhaseSimplex();
 			//read("./LP_problems/BasicExample.csv");
 		} else {
 			//vogel test
@@ -20,79 +22,117 @@ public class Source {
 	private static void testTwoPhaseSimplex() {
 		double[][] table = { { -1, -1, 18 }, { -1, 0, 11 }, { 0, -1, 10 }, { 1, 1, -9 }, { -1, -3, 229 } };
 		Simplex simple = new Simplex(table, false);
-		System.out.println(simple.solve());
+		double solution = simple.solve();
+
+		if (solution == 191)
+			System.out.print("pass: ");
+		else
+			System.out.print("!!!!!!FALSE!!!!!! ");
+
+		System.out.println(solution);
 	}
 
 	private static void testSimpleSimplex() {
 		double[][] table = { { -1, -1, 40 }, { -40, -120, 2400 }, { -7, -12, 312 }, { 100, 250, 0 } };
 		Simplex simple = new Simplex(table, true);
-		System.out.println(simple.solve());
+		double solution = simple.solve();
+
+		if (solution == 5400)
+			System.out.print("pass: ");
+		else
+			System.out.print("!!!!!!FALSE!!!!!! ");
+
+		System.out.println(solution);
 	}
 
 	private static void read(String filePath) {
 		Scanner s = null;
 
+		boolean isMax = false;
+		double[][] table = null;
+
 		BufferedReader in;
+
 		try {
-			String line;
-			int correction = 0;
 			int cols;
 			int rows;
 			boolean[] canBeNeg;
 
-			boolean isMax;
-			double[][] table;
-
 			in = new BufferedReader(new FileReader(filePath));
-			in.readLine();
-			in.readLine();
-			in.readLine();
-
-			while ((line = in.readLine()) != null)
-				if (line.startsWith("="))
-					correction++;
-
+			int correction = getCorrection(in);
 			in.close();
 			in = new BufferedReader(new FileReader(filePath)); //reopen
 
-			cols = s.nextInt() + 1;
-			rows = s.nextInt() + correction + 1;
+			String[] tmp = in.readLine().split(";");
+			cols = Integer.parseInt(tmp[0]) + 1;
+			rows = Integer.parseInt(tmp[1]) + correction + 1;
 			table = new double[rows][cols];
-			isMax = s.next().contains("max");
-			//s.next?
 
-			//read target function, if we should minimize the function, multiply with -1
-			for (int i = 0; i < cols; i++)
-				if (isMax)
-					table[i][rows - 1] = s.nextDouble();
-				else
-					table[i][rows - 1] = -s.nextDouble();
-
-			canBeNeg = new boolean[cols];
-			for (int i = 0; i < cols; i++)
-				canBeNeg[i] = s.nextBoolean();
-
-			//read table
-			for (int i = 0; i < rows - 1; i++) {
-				String nextLine = s.next();
-				if (nextLine.startsWith("=")) {
-					for (int j = 0; j < cols; j++) {
-						double input = s.nextDouble();
-						table[i][j] = input;
-						table[i + 1][j] = input; //missing >= , *(-1)??
-					}
-					i++;
-
-				} else {
-					for (int j = 0; j < cols; j++)
-						table[i][j] = s.nextDouble();
-				}
-			}
+			isMax = readTargetFunction(in, table, rows, cols);
+			//read canbeNeg
+			//			canBeNeg = new boolean[cols];
+			//			for (int i = 0; i < cols; i++)
+			//				canBeNeg[i] = s.nextBoolean();
+			in.readLine();
+			readTable(in, table, rows, cols);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+		if (table != null) {
+			System.out.println("bla");
+		}
+
+	}
+
+	private static int getCorrection(BufferedReader in) throws IOException {
+		int out = 0;
+		in.readLine();
+		in.readLine();
+		in.readLine();
+
+		String line;
+		while ((line = in.readLine()) != null)
+			if (line.startsWith("="))
+				out++;
+
+		return out;
+	}
+
+	private static boolean readTargetFunction(BufferedReader in, double[][] table, int rows, int cols) throws IOException {
+		String[] tmp = in.readLine().split(";");
+		boolean isMax = tmp[0].contains("max");
+
+		for (int i = 0; i < cols; i++) {
+			double input = Double.parseDouble(tmp[i + 1]);
+			table[rows - 1][i] = input;
+		}
+
+		return isMax;
+	}
+
+	private static void readTable(BufferedReader in, double[][] table, int rows, int cols) throws IOException {
+		for (int i = 0; i < rows - 1; i++) {
+			String[] tmp = in.readLine().split(";");
+
+			if (tmp[0].startsWith("=")) {
+				for (int j = 0; j < cols; j++) {
+					double input = Double.parseDouble(tmp[j + 1]);
+					table[i][j] = input;
+					table[i + 1][j] = -input; //missing >= , *(-1)??
+				}
+				i++;
+
+			} else {
+				boolean isGreater = tmp[0].startsWith("<");
+
+				for (int j = 0; j < cols; j++) {
+					double input = Double.parseDouble(tmp[j + 1]);
+					table[i][j] = isGreater ? input : -input;
+				}
+			}
+		}
 	}
 
 }
